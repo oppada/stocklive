bc0ed32e0cd81cdb5c7953363edfb90beaccdec0
oppada <hp01075531390@gmail.com>
2026-01-30 17:59:43 +0900

    260130 1757

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { LineChart, Line, ResponsiveContainer, YAxis } from 'recharts';
 import { Activity } from 'lucide-react';
 import { Link } from 'react-router-dom';
